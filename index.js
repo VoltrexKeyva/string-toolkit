be31d7faca0ac8596e0ec08cd470fc8978e9bcb3
VoltrexMaster <62040526+VoltrexMaster@users.noreply.github.com>
2021-01-02 20:17:39 +0330

    Cleanups, improvements and optimizations
    Cleaned up some code, improved some methods and optimized efficiency.

--- index.js
+++ index.js
@@ -1,43 +1,35 @@
 class Functions {
   constructor() {
-    this.version = require("./package.json").version;
+    this.version = require('./package.json').version;
   }
 
   /**
    * Proper cases a string.
-   * @param {string} string - String to proper case
-   * @param {?boolean} lowerCaseBoolean - Whether or not to cast the string into lowercase before proper casing it
+   * @param {string} string String to proper case
+   * @param {boolean?} [lowerCaseBoolean] Whether or not to cast the string into lowercase before proper casing it
    * @returns {string}
    */
   toProperCase(string, lowerCaseBoolean) {
-    if (!string) {
-      throw new TypeError("Expected a string.");
-    }
+    if (typeof string !== 'string') throw new TypeError('First parameter must be a type of string');
 
-    return ((!lowerCaseBoolean || lowerCaseBoolean === false || lowerCaseBoolean !== true) ? string.toString() : string.toString().toLowerCase()).replace(/(\b\w)/gi, w => w.toUpperCase());
+    return (lowerCaseBoolean !== true ? string : string.toLowerCase()).replace(/(\b\w)/gi, w => w.toUpperCase());
   }
 
   /**
    * Chunks a string to the specified amount of chars.
-   * @param {string} string - String to chunk
-   * @param {number} ChunkBy - Amount of chars to chunk by
+   * @param {string} string String to chunk
+   * @param {number} ChunkBy Amount of chars to chunk by
    * @returns {string[]}
    */
   toChunks(string, ChunkBy) {
-    if (!string) {
-      throw new TypeError("Expected a string.");
-    }
+    if (typeof string !== 'string') throw new TypeError('First parameter must be a type of string');
 
-    if (!ChunkBy || !Number.isInteger(ChunkBy)) {
-      throw new TypeError("Expected a char split position in the string as the second parameter, and it must be a number.");
-    }
-
-    let stringToChunk = string.toString();
-
-    let chunksNum = Math.ceil(stringToChunk.length / parseInt(ChunkBy));
+    if (!Number.isInteger(ChunkBy)) throw new TypeError('Second parameter must be a type of number');
+    
+    let chunksNum = Math.ceil(string.length / ChunkBy);
     let chunks = new Array(chunksNum);
-    for (let i = 0, o = 0; i < parseInt(chunksNum); ++i, o += parseInt(ChunkBy)) {
-      chunks[i] = stringToChunk.substr(o, parseInt(ChunkBy));
+    for (let i = 0, o = 0; i < chunksNum; ++i, o += ChunkBy) {
+      chunks[i] = string.substr(o, ChunkBy);
     }
 
     return chunks;
@@ -45,13 +37,11 @@ class Functions {
 
   /**
    * Scrambles a string.
-   * @param {string} string - String to scramble
+   * @param {string} string String to scramble
    * @returns {string}
    */
   scramble(string) {
-    if (!string) {
-      throw new TypeError("Expected a string.");
-    }
+    if (typeof string !== 'string') throw new TypeError('First parameter must be a type of string');
 
     function shuffle(array) {
       let currentIndex, randomIndex, tempIndex;
@@ -67,117 +57,98 @@ class Functions {
       return array;
     }
 
-    return shuffle([...string.toString()]).join("");
+    return shuffle(string.split('')).join('');
   }
 
   /**
    * Mocks a string.
-   * @param {string} string - String to mock
+   * @param {string} string String to mock
    * @returns {string}
    */
   mock(string) {
-    if (!string) {
-      throw new TypeError("Expected a string.");
-    }
-
-    let chunksFunction = this.toChunks;
-
-    return chunksFunction(string.toString().toLowerCase(), 2).map(e => e.slice(0, 1) + e.slice(1).toUpperCase()).join("");
+    if (typeof string !== 'string') throw new TypeError('First parameter must be a type of string');
+    
+    return this.toChunks(string.toLowerCase(), 2).map(e => e.slice(0, 1) + e.slice(1).toUpperCase()).join('');
   }
 
   /**
    * Emojifies a string.
-   * @param {string} string - String to emojify
+   * @param {string} string String to emojify
    * @returns {string}
    */
   emojify(string) {
-    if (!string) {
-      throw new TypeError("Expected a string.");
-    }
+    if (typeof string !== 'string') throw new TypeError('First parameter must be a type of string');
 
     let specialObj = {
-      0: ":zero:",
-      1: ":one:",
-      2: ":two:",
-      3: ":three:",
-      4: ":four:",
-      5: ":five:",
-      6: ":six:",
-      7: ":seven:",
-      8: ":eight:",
-      9: ":nine:",
-      " ": " ",
-      "!": ":exclamation:",
-      "?": ":question:"
+      0: ':zero:',
+      1: ':one:',
+      2: ':two:',
+      3: ':three:',
+      4: ':four:',
+      5: ':five:',
+      6: ':six:',
+      7: ':seven:',
+      8: ':eight:',
+      9: ':nine:',
+      10: ':keycap_ten:',
+      ' ': ' ',
+      '!': ':exclamation:',
+      '?': ':question:'
     };
 
-    return [...string.toString()].map(e => {
-      if (e.match(/\W/) || e.match(/\d/)) {
-        return specialObj[e] || e;
-      } else if (e.match(/\w/)) {
-        return `:regional_indicator_${e.toLowerCase()}:`;
-      }
-    }).join("");
+    return string.toLowerCase().split('').reduce((acc, current) => acc + (specialObj[current] || (/\w/.test(current) ? `:regional_indicator_${current}:` : undefined) || current), '');
   }
 
   /**
    * This function is related to discord, checks if the string contains a custom emoji.
-   * @param {string} string - String to check whether or not it has a custom emoji
-   * @returns {(number|"No custom emoji detected")}
+   * @param {string} string String to check whether or not it has a custom emoji
+   * @returns {boolean}
    */
   hasCustomEmoji(string) {
-    if (!string) {
-      throw new TypeError("Expected a string.");
-    }
+    if (typeof string !== 'string') throw new TypeError('First parameter must be a type of string');
 
-    return string.toString().match(/<a?:(\w{2,32}):(\d{17,19})>/) ? string.toString().match(/<a?:(\w{2,32}):(\d{17,19})>/gi).length : "No custom emoji detected";
+    return /<a?:(\w{2,32}):(\d{17,19})>/.test(string);
   }
 
   /**
    * Creates a progress bar.
-   * @param {number} inTotal - Elapsed
-   * @param {number} Total - Goal
+   * @param {number} inTotal Elapsed
+   * @param {number} Total Goal
    * @typedef {Object} progressBarOptions
-   * @property {string} elapsedChar - Character to fill the elapsed portion of the progress bar
-   * @property {string} progressChar - Character for the current progress
-   * @property {string} emptyChar - Character to fill the empty portion of the progress bar or in other words, the unreached portion
-   * @property {number} barLength - Length of the progress bar in chars
-   * @param {?progressBarOptions} options - Options for the progress bar
+   * @property {string} elapsedChar Character to fill the elapsed portion of the progress bar
+   * @property {string} progressChar Character for the current progress
+   * @property {string} emptyChar Character to fill the empty portion of the progress bar or in other words, the unreached portion
+   * @property {number} barLength Length of the progress bar in chars
+   * @param {progressBarOptions?} [options] Options for the progress bar
    * @returns {string}
    */
   createProgressBar(inTotal, Total, options = {}) {
-    if (!Number.isInteger(inTotal) || !Number.isInteger(Total)) {
-      throw new TypeError("Both the first and the second parameters are required and must be typeof number.");
-    }
+    if (!Number.isInteger(inTotal) || !Number.isInteger(Total)) throw new TypeError('the first and the second parameters are required and must be a type of number.');
 
-    if (parseInt(inTotal) > parseInt(Total)) {
-      throw new RangeError("First parameter must be lesser than the second parameter");
-    }
+    if (inTotal > Total) throw new RangeError('First parameter must be lesser than the second parameter');
 
     options = {
-      elapsedChar: options.elapsedChar || "=",
-      progressChar: options.progressChar || ">",
-      emptyChar: options.emptyChar || "-",
+      elapsedChar: typeof options.elapsedChar === 'string' ? options.elapsedChar : '=',
+      progressChar: typeof options.progressChar === 'string' ? options.progressChar : '>',
+      emptyChar: typeof options.emptyChar === 'string' ? options.emptyChar : '-',
       barLength: Number.isInteger(options.barLength) ? options.barLength : 50
     };
 
     let available = (inTotal / Total) * options.barLength;
 
-    let progressBar = options.elapsedChar.toString().repeat(available) + options.progressChar.toString() + options.emptyChar.toString().repeat(options.barLength - (available + (inTotal === Total ? 0 : 1)));
+    let progressBar = options.elapsedChar.repeat(available) + options.progressChar + options.emptyChar.repeat(options.barLength - (available + (inTotal === Total ? 0 : 1)));
     return progressBar.length > options.barLength ? progressBar.slice(0, options.barLength) : progressBar;
   }
 
   /**
    * Gets the abbreviation of a string.
-   * @param {string} string - String to get an abbreviation of
+   * @param {string} string String to get an abbreviation of
    * @returns {string}
    */
   toAbbreviation(string) {
-    if (!string) {
-      throw new TypeError("Expected a string.");
-    }
+    if (typeof string !== 'string') throw new TypeError('First parameter must be a type of string');
 
-    return string.toString().includes(" ") ? string.toString().trim().split(" ").map(element => element.charAt(0)).join("") : string.toString();
+    return string.includes(' ') ? string.trim().split(' ').map(element => element[0]).join('') : string;
   }
 
   /**
@@ -186,7 +157,7 @@ class Functions {
    */
   fakeToken() {
     let IDs = [17, 18, 19],
-      chars = [..."abcdefghijklmnopqrstuvwxyz1234567890"],
+      chars = [...'abcdefghijklmnopqrstuvwxyz1234567890'],
       options = [0, 1];
     return (Buffer.from(Array.from({
       length: IDs[Math.floor(Math.random() * IDs.length)]
@@ -195,55 +166,64 @@ class Functions {
         length: 10
       }, (a, r) => r);
       return nums[Math.floor(Math.random() * nums.length)];
-    }).join("")).toString("base64") + "." + Array.from({
-      length: 33
-    }, (a, r) => r === 5 ? "." : chars[Math.floor(Math.random() * chars.length)]).reduce((acc, current) => (acc += options[Math.floor(Math.random() * options.length)] === 1 ? current.toUpperCase() : current, acc), "")).slice(0, 59);
+    }).join('')).toString('base64') + '.' + Array.from({
+      length: 50
+    }, (a, r) => r === 5 ? '.' : chars[Math.floor(Math.random() * chars.length)]).reduce((acc, current) => (acc += options[Math.floor(Math.random() * options.length)] === 1 ? current.toUpperCase() : current, acc), '')).slice(0, 59);
   }
 
   /**
    * Shortens a string by a specified amount
-   * @param {string} string - String to shorten
-   * @param {number} length - Amount of chars to shorten by
-   * @param {?string} placeholder - The string to concatenate to be shortened string
+   * @param {string} string String to shorten
+   * @param {number} length Amount of chars to shorten by
+   * @param {string?} [placeholder] The string to concatenate to be shortened string
    * @returns {string}
    */
   shorten(string, length, placeholder) {
-    if (typeof string !== "string") throw new TypeError("First parameter must be a type of string");
-    if (!Number.isInteger(length)) throw new TypeError("Second parameter must be a type of number");
-    return string.length > length ? string.slice(0, length) + (placeholder ? placeholder.toString() : "...") : string;
+    if (typeof string !== 'string') throw new TypeError('First parameter must be a type of string');
+    if (!Number.isInteger(length)) throw new TypeError('Second parameter must be a type of number');
+    return string.length > length ? string.slice(0, length) + (placeholder ? placeholder.toString() : '...') : string;
   }
 
   /**
    * Parses options and flags from an array of strings.
-   * @param {string[]} args - Array of strings to parse options and flags from.
+   * @param {string[]} args Array of strings to parse options and flags from.
    * @typedef {Object} OptionsAndFlagsObject
-   * @property {Object} OptionsAndFlagsObject.options - All the parsed options.
-   * @property {string[]} OptionsAndFlagsObject.flags - All the parsed flags.
-   * @property {string} OptionsAndFlagsObject.contentNoOptions - All the provided strings in the array concatenated without the options.
-   * @property {string} OptionsAndFlagsObject.contentNoFlags - All the provided strings in the array concatenated without the flags.
+   * @property {Object} OptionsAndFlagsObject.options All the parsed options.
+   * @property {string[]} OptionsAndFlagsObject.flags All the parsed flags.
+   * @property {string} OptionsAndFlagsObject.contentNoOptions All the provided strings in the array concatenated without the options.
+   * @property {string} OptionsAndFlagsObject.contentNoFlags All the provided strings in the array concatenated without the flags.
    * @returns {OptionsAndFlagsObject}
    */
   parseOptions(args) {
-    if (!Array.isArray(args) || !args.every(argument => typeof argument === "string")) throw new TypeError("First parameter must be an array and every element must be a type of string.");
+    if (!Array.isArray(args) || !args.every(argument => typeof argument === 'string')) throw new TypeError('First parameter must be an array and every element must be a type of string');
 
-    let matches = args.filter(a => a.match(/--(.*)/));
-
-    if (!matches.length) return {
-      options: {},
-      flags: [],
-      contentNoOptions: args.join(" "),
-      contentNoFlags: args.join(" ")
+    let matches = args.filter(a => a.startsWith('--')),
+    joined = args.join(' '),
+    output = {
+       options: {},
+       flags: [],
+       contentNoOptions: joined,
+       contentNoFlags: joined
     };
-
-    let optionsAndFlags = Object.entries(matches.map(match => args.slice(args.indexOf(match))).map(m => m.slice(1)).reduce((acc, current, i) => (acc[matches[i].slice(2)] = (current.reduce((a, b) => ((a[1] ? a : (b.match(/--(.*)/) ? (a[1] = true, a) : a[0].push(b))), a), [[], false])[0].join(" ")), acc), {})).reduce((acc, current) => (current[1] ? acc.options[current[0]] = current[1] : acc.flags.push(current[0]), acc), {
-      options: {},
-      flags: []
-    });
-
-    optionsAndFlags["contentNoOptions"] = args.join(" ").indexOf(matches[0]) === 0 ? "" : args.join(" ").slice(0, args.join(" ").indexOf(matches[0]) - 1);
-    optionsAndFlags["contentNoFlags"] = matches.reduce((acc, current) => acc.replace(new RegExp(current, "g"), ""), args.join(" ")).replace(/ +/g, " ");
-    return optionsAndFlags;
-  }
+    if (!matches.length) return output;
+    for (let match of matches) {
+       let m = args.slice(args.indexOf(match) + 1),
+       s = [];
+       for (let index of m) {
+          if (index.startsWith('--')) break;
+          s.push(index);
+       }
+       if (s.length) {
+          output.options[match.slice(2)] = s.join(' ');
+       } else {
+          output.flags.push(match.slice(2));
+       }
+    }
+    let x = joined.indexOf(matches[0]);
+    output.contentNoOptions = x <= 0 ? '' : joined.slice(0, x - 1);
+    output.contentNoFlags = x === -1 ? '' : args.filter(arg => !arg.startsWith('--')).join(' ');
+    return output;
+}
 
   // By Voltrex Master
 }
